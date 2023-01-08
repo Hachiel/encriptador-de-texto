52d138c1cdcee62f85f916e2c1271146af4263d7
Hachiel <ivan.venegoni@gmail.com>
2023-01-08 14:32:46 +0900

    Agregadas funciones de cipher y descipher

--- index.js
+++ index.js
@@ -1,36 +1,88 @@
-let a = "al";
-let e = "enter";
-let i = "imes";
-let o = "ober";
-let u = "ufat";
+window.onload = function(){
+    document.getElementById("button_cipher").onclick=cipher;
+    document.getElementById("button_decipher").onclick=decipher;
+    document.getElementById("copy_text").onclick=copyText;
+}
 
-function copiarTexto(texto) {
-    let areaTexto = document.createElement('textarea');
-    areaTexto.value = texto;
-    areaTexto.setAttribute('readonly', '');
-    areaTexto.style.position = 'absolute';
-    areaTexto.style.left = '-999px';
+function keyWord(vocals){
+    switch(vocals){
+        case "a":
+            return "ai";
+        case "e":
+            return "enter";
+        case "i":
+            return "imes";
+        case "o":
+            return "ober";
+        case "u":
+            return "ufat";
+    }
+}
 
-    document.body.appendChild(areaTexto);
+function cipher(){
+    var dataEntry = document.getElementById("boxIn1").value;
+    var dataOuput = document.getElementById("boxOut1");
+    var content = dataEntry.toLowerCase();
+    var result = "";
 
-    let selector = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+    for(i=0; i<content.length; i++){
+        if (content[i] == "a"){
+            result += keyWord(content[i])
+        }
+        else if (content[i] == "e"){
+            result += keyWord(content[i])
+        }
+        else if (content[i] == "i"){
+            result += keyWord(content[i])
+        }
+        else if (content[i] == "o"){
+            result += keyWord(content[i])
+        }
+        else if (content[i] == "u"){
+            result += keyWord(content[i])
+        } else {
+            result += content[i];
+        }
+    }
+    dataOuput.innerHTML = result;
+}
 
-    areaTexto.select();
+function decipher(){
+    var dataEntry = document.getElementById("boxIn1").value;
+    var dataOuput = document.getElementById("boxOut1");
+    var content = dataEntry.toLowerCase();
+    var result = "";
 
+    for(i=0; i<content.length; i++){
+        if (content[i] == "a"){
+            result += "a";
+            i += 1;
+        }
+        else if (content[i] == "e"){
+            result += "e";
+            i += 4;
+        }
+        else if (content[i] == "i"){
+            result += "i";
+            i += 3;
+        }
+        else if (content[i] == "o"){
+            result += "o";
+            i += 3;
+        }
+        else if (content[i] == "u"){
+            result += "u";
+            i += 3;
+        } else {
+            result += content[i];
+        }
+    }
+    dataOuput.innerHTML = result;
+}
+
+function copyText(){
+    var dataEntry = document.getElementById("boxOut1");
+    //No pude encontrar la forma de hacerlo con la API clipboard.
+    dataEntry.select()
     document.execCommand('copy');
-
-    document.body.removeChild(areaTexto);
-
-    if(selector) {
-        document.getSelection().removeAllRanges();
-        document.getSelection().addRange(selector);
-    }
-    
-    window.onload = function() {
-        document.getElementById('copiar').addEventListener('click', () => {
-            let contenido = document.getElementById('cajaE1').value;
-
-            copiarTexto(contenido);
-        });
-    }
 }
